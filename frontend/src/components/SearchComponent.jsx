db59b4d2fe13e94701565dd07bb98482bfff583e
Ayan Chowdhury <Ayan@Ayans-MacBook-Pro.local>
2023-12-06 00:05:41 -0700

    initial repo setup, adding frontend, backend (firestore and api), vm (Dockerfile and YAML)

---
+++ frontend/src/components/SearchComponent.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+function SearchComponent({ onSearch }) {
+  const [query, setQuery] = useState('');
+
+  const handleSearch = () => {
+    if (query) {
+      onSearch(query);
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        placeholder="Search for rentals..."
+      />
+      <button onClick={handleSearch}>Search</button>
+    </div>
+  );
+}
+
+export default SearchComponent;
