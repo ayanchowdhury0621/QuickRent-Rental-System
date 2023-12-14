470e69996142ca06cf6e875fabf5fe3e10b0b45d
arindrajit-paul <arpa6911@colorado.edu>
2023-12-14 00:00:39 -0700

    Connect to GCP

--- frontend/src/components/AddRentals.js
+++ frontend/src/components/AddRentals.js
@@ -22,8 +22,6 @@ function AddRentals() {
 
     uploadTask.on('state_changed',
       (snapshot) => {
-        // Observe state change events such as progress, pause, and resume
-        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
         const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
         console.log('Upload is ' + progress + '% done');
         switch (snapshot.state) {
@@ -34,6 +32,7 @@ function AddRentals() {
             console.log('Upload is running');
             break;
         }
+        // need to let the user know when the upload finishes
       },
       (error) => {
         // Handle unsuccessful uploads
@@ -47,15 +46,6 @@ function AddRentals() {
         });
       }
     );
-
-    // uploadBytes(imgRef, imgUpload).then(() => {
-    //   alert("Image uploaded successfully");
-    // })
-    // getDownloadURL(imgRef).then((url) => {
-    //   setImgUrl(url);
-    // })
-
-    // console.log(imgUrl);
   }
 
   const handleClick = async (e) => {
--- frontend/src/components/config/firebase_storage.js
+++ frontend/src/components/config/firebase_storage.js
@@ -1,24 +1,19 @@
-// Import the functions you need from the SDKs you need
-import {initializeApp} from "firebase/app";
+import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA17F0336JMPhKG5jgxTVAyjgeHSkiAbOs",
-  authDomain: "quickrent-rental-system-4b137.firebaseapp.com",
-  projectId: "quickrent-rental-system-4b137",
-  storageBucket: "quickrent-rental-system-4b137.appspot.com",
-  messagingSenderId: "984287518782",
-  appId: "1:984287518782:web:e327dcb2de1d20a05db16f",
-  measurementId: "G-6YPB9WKZ18"
+  apiKey: "AIzaSyDgsmXIpOsATAWsYEzeuGQsXfDQx97mxoQ",
+  authDomain: "quickrent-rental-system-407522.firebaseapp.com",
+  projectId: "quickrent-rental-system-407522",
+  storageBucket: "quickrent-rental-system-407522.appspot.com",
+  messagingSenderId: "241420795370",
+  appId: "1:241420795370:web:78cc7531546ee5927dc693"
 };
 
-
 // Initialize Firebase
-
 const app = initializeApp(firebaseConfig);
 const imgStorage = getStorage(app);
 const docStorage = getFirestore(app);
-
 
 export { imgStorage, docStorage };
